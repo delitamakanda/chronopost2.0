94a3b1798a7bba02bb45a309e4c2b9fdc7284a25
Délita Makanda <delita.makanda@gmail.com>
2023-08-13 02:49:50 +0200

    feat(app): add router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
 import CustomersScreen from 'screens/customers';
 import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import OrdersScreen from 'screens/orders';
 
 function App() {
   return (
-    <CustomersScreen />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/orders" />} />
+        <Route path="/orders" element={<OrdersScreen />} />
+        <Route path="/customers" element={<CustomersScreen />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
