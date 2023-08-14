830a7cc96e9cdb4645352a1de59dc96bd876c357
Délita Makanda <delita.makanda@gmail.com>
2023-08-14 17:42:35 +0200

    feat(api): add get order by id endpoint

--- tracker/queries.py
+++ tracker/queries.py
@@ -8,6 +8,10 @@ class Query(graphene.ObjectType):
     all_orders = graphene.List(OrderType)
     all_orders_items = graphene.List(OrderItemType)
     all_orders_by_customer = graphene.List(OrderType, customer_id=graphene.String())
+    get_order_by_id = graphene.Field(OrderType, order_id=graphene.String(required=True))
+
+    def resolve_get_order_by_id(self, info, order_id):
+        return Order.objects.get(id=order_id)
 
     def resolve_all_orders_by_customer(self, info, customer_id):
         return Order.objects.filter(customer__id__iexact=customer_id)
